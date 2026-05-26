cd4e848b8f532df34f3351590fecd15e151333b8
jerrywuhi <wu0952686160@gmail.com>
2026-05-26 11:24:05 +0800

    Add files via upload

--- web1.py
+++ web1.py
@@ -157,8 +157,12 @@ def webhook3():
                 result += "片名：" + dict["title"] + "\n"
                 result += "介紹：" + dict["hyperlink"] + "\n\n"
         info += result
+    elif (action == "input.unknown"):
+        info =  req["queryResult"]["queryText"]
+        
     return make_response(jsonify({"fulfillmentText": info}))
 
+    
 
 
 @app.route("/rate")
