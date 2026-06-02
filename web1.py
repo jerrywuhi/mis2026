f7e352cc97b6d47eda0c7573560fb4f457059d38
jerrywuhi <wu0952686160@gmail.com>
2026-06-02 11:46:22 +0800

    Add files via upload

--- web1.py
+++ web1.py
@@ -164,8 +164,6 @@ def webhook3():
             "你是一個熱心且知識豐富的專業智慧助理。"
             "對於使用者的提問，請回覆重點的關鍵字，不要重述問題。"         
         )
-
-
         ai_config = types.GenerateContentConfig(
             max_output_tokens=500, 
             system_instruction=instruction_text
